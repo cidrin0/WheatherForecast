1c642d84791d821cc9a1b3927b519090aab3f620
735395 <henrique.cidrin@iteris.local>
2019-10-09 18:30:12 -0300

    adicionado city

---
+++ src/main/java/br/usjt/wheatherForecast/model/entity/City.java
@@ -0,0 +1,45 @@
+package br.usjt.wheatherForecast.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+
+@Data
+@Entity
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    @NotNull
+    private String name = "";
+
+    @NotNull
+    @Min(Long.MIN_VALUE)
+    @Max(Long.MAX_VALUE)
+    private Long latitude;
+
+    @NotNull
+    @Min(Long.MIN_VALUE)
+    @Max(Long.MAX_VALUE)
+    private Long longitude;
+
+/*
+    @OneToMany(mappedBy = "id", targetEntity = WheatherForecast.class)
+    private Collection<WheatherForecast> forecasts;
+*/
+
+}
